f0c0e25c2d3aca9eaec5c8c47492f80dec526206
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-03-07 18:06:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+
+const currentUser = 'dawg';
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + " !";
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const firstInitial = currentUser[0];
+
+let shortGreeting = "Welcome, "+ firstInitial + "!";
